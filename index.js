2ce79eddcea80d0823ad7e807c42d1446320c6b8
hzamanian58 <91015046+hzamanian58@users.noreply.github.com>
2023-05-10 17:45:23 +0330

    Create index.js
    http server test on port 5000 is created

---
+++ index.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+//const hostname = 'localhost';
+const hostname = '192.168.222.134';
+const port = 5000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end("ooooppppssssseeee!\nthis site can't be reach now,\nplease try again later. Thanks \n");
+
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
